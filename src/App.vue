9c6395ed7708cb821d880f7149d531d4f7f363d0
suchawadee-s <suchawadee.s@grouplease.co.th>
2018-03-12 23:02:31 +0700

    init simple table

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+   <div> New Employee Registration</div>
+  <AppTable/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import AppTable from './components/AppTable.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AppTable
   }
 }
 </script>
@@ -25,4 +25,61 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+ element.style {
+        background-color: rgb(10, 47, 88);
+    }
+
+    body {
+        font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+        margin: 0;
+        display: flex;
+        min-height: 100vh;
+        flex-direction: column;
+    }
+
+    .el-menu, body, html {
+        height: 100%;
+    }
+
+    .wrapper {
+
+        position: relative;
+    }
+
+    footer, div {
+        display: block;
+    }
+
+    .container {
+        padding-top: 70px;
+        flex: 1;
+    }
+
+    .container, .wrapper {
+        height: 100%;
+    }
+
+    .menu {
+        height: 100%;
+        background-color: #eef1f6;
+    }
+
+    .content {
+        padding-top: 25px;
+        padding-right: 25px;
+        padding-bottom: 125px;
+        padding-left: 25px;
+    }
+
+    .footer {
+        height: 120px;
+        background-color: #324057;
+        color: #a4aebd;
+        width: 100%;
+        z-index: 1000;
+        margin-top: -120px;
+        line-height: 1;
+        font-size: 22px;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,21 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+
+import {
+  Table,
+  TableColumn
+} from 'element-ui'
+import App from './App.vue'
+import 'element-theme-default'
 
 Vue.config.productionTip = false
 
+Vue.use(Table)
+Vue.use(TableColumn)
+
 /* eslint-disable no-new */
+// eslint-disable-next-line
 new Vue({
   el: '#app',
   components: { App },
