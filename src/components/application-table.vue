00050cefed42a78f85e3e75ec489441236975fd6
suchawadee-s <suchawadee.s@grouplease.co.th>
2018-03-23 12:04:00 +0700

    use bootstrapVue component to format table

--- src/components/application-table.vue
+++ src/components/application-table.vue
@@ -1,11 +1,25 @@
 <template>
-    <div>
-<b-table striped hover head-variant="dark" :items="tableData" :fields="fields" ></b-table>
-    </div>
+  <div>
+    <b-row>
+      <b-col>
+       <b-form-input v-model="searchText" type="text" placeholder="Search"></b-form-input>
+      </b-col>
+      <b-col lg="6">
+      </b-col>
+    </b-row>
+
+    <b-table striped hover head-variant="dark" :items="tableData" :fields="fields" ></b-table>
+    <b-row>
+      <b-col>Total: {{total}} employees</b-col>
+      <b-col lg="6">
+      <b-pagination size="sm" :total-rows="total" v-model="currentPage" :per-page="pageSize"></b-pagination>
+     </b-col>
+    </b-row>
+  </div>
 </template>
 
 <script>
-import {AXIOS} from './http-common'
+import { AXIOS } from './http-common'
 
 export default {
   data () {
@@ -14,7 +28,8 @@ export default {
       tableData: [],
       total: 0,
       pageSize: 10,
-      currentPage: 0,
+      currentPage: 1,
+      searchText: '',
       dialogFormVisible: false,
       form: ''
     }
@@ -27,7 +42,7 @@ export default {
     getApplicationData () {
       AXIOS.get('employment-applications', {
         params: {
-          page: this.currentPage,
+          page: this.currentPage - 1,
           pageSize: this.pageSize
         }
       })
@@ -36,6 +51,7 @@ export default {
           this.response = response.data
           console.log(response.data)
           this.tableData = response.data
+          this.total = response.data.length
           this.httpStatusCode = response.status
           this.httpStatusText = response.statusText
           this.headers = response.headers
