71d4314996a9cb5568dec839d6c55aed91e19aaa
suchawadee-s <suchawadee.s@grouplease.co.th>
2018-03-24 10:24:00 +0700

    remove pagination

--- src/components/ApplicationTable.vue
+++ src/components/ApplicationTable.vue
@@ -10,16 +10,10 @@
     </b-row>
 
     <b-table striped hover head-variant="dark" :items="tableData" :fields="fields" >
-      <template slot="edit-details" slot-scope="row">
-        <b-button size="sm" @click="editItem(row.$index,tableData)" ></b-button>
+      <template slot="detail" slot-scope="data">
+        <b-button size="sm" @click="editItem(data.index,tableData)"></b-button>
       </template>
     </b-table>
-    <b-row>
-      <b-col>Total: {{total}} employees</b-col>
-      <b-col lg="6">
-      <b-pagination size="sm" :total-rows="total" v-model="currentPage" :per-page="pageSize"></b-pagination>
-     </b-col>
-    </b-row>
     <!-- <application-detail :show="dialogFormVisible" :application="application"></application-detail> -->
   </div>
 </template>
@@ -31,11 +25,10 @@ import { AXIOS } from './http-common'
 export default {
   data () {
     return {
-      fields: ['firstName', 'lastName', 'email', 'address', 'edit_details'],
+      fields: ['firstName', 'lastName', 'email', 'address', 'detail'],
       tableData: [],
-      total: 0,
-      pageSize: 10,
-      currentPage: 1,
+      limit: 10,
+      offset: 0,
       searchText: '',
       dialogFormVisible: false,
       application: '',
@@ -72,7 +65,7 @@ export default {
     },
     search (event) {
       console.log('searching data...')
-      let params = {page: this.currentPage - 1, pageSize: this.pageSize}
+      let params = {offset: this.offset, limit: this.limit}
 
       if (this.searchText.trim().length > 0 && this.searchBy.length > 0) {
         console.log(this.searchText)
