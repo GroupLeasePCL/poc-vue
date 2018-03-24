b399841a1594552d1e40782751ec32455fb79b27
Peeranut <peeranut.ng@grouplease.co.th>
2018-03-24 12:08:46 +0700

    [NavigationBar] Implemented NavigationBar component

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,7 +1,31 @@
 <template>
-  <div>
-    <h1>{{ msg }}</h1>
-  </div>
+  <b-navbar toggleable="md" type="dark" fixed="top" class="bg">
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+    <b-navbar-brand href="#">
+      <img src="../assets/gl_logo_rgb.png" class="d-inline-block align-top" width="150px"/>
+    </b-navbar-brand>
+    <b-nav-text class="line-2"/>
+
+    <b-collapse is-nav id="nav_collapse">
+      <b-navbar-nav>
+        <b-nav-item href="#">Manu 01</b-nav-item>
+        <b-nav-item href="#">Menu 02</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown right>
+          <!-- Using button-content slot -->
+          <template slot="button-content">
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Signout</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
 </template>
 
 <script>
@@ -20,4 +44,18 @@ export default {
 h1, h2 {
   font-weight: normal;
 }
+
+.line-2 {
+  box-sizing: border-box;
+  height: 33px;
+  width: 1px;
+  border: 1px solid #FFFFFF;
+}
+
+.bg {
+  height: 70px;
+  width: 100%;
+  background: linear-gradient(90deg, #8BC53F 0%, #069DD8 100%);
+}
+
 </style>
