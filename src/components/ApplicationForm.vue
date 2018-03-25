2cee2ec2493b5944e55b6d374072c085d7088a24
Peeranut <peeranut.ng@grouplease.co.th>
2018-03-25 09:39:48 +0700

    [SplitDetailComponent] Clean up components

--- src/components/ApplicationForm.vue
+++ src/components/ApplicationForm.vue
@@ -1,13 +1,13 @@
 <template>
-    <div role='tablist'>
+    <div>
         <b-card no-body class='mb-1'>
-            <b-card-header header-tag='header' class='p-1' role='tab'>
-                <b-btn block href='#' v-b-toggle.accordion1 variant='light'>Employee Information</b-btn>
+            <b-card-header header-tag='header' class='p-1'>
+              <b-btn block href='#' v-b-toggle.employmentApplication variant='light'>Employment Information</b-btn>
             </b-card-header>
 
-            <b-collapse id='accordion1' visible accordion='my-accordion' role='tabpanel'>
+            <b-collapse id='employmentApplication'>
                 <b-card-body>
-                    <b-form @submit='onSubmit' @reset='onReset' v-if='show'>
+                    <b-form @submit='onSubmit' @reset='onReset'>
                         <PersonalInformationForm></PersonalInformationForm>
                         <div class='line-2'/>
 
@@ -16,8 +16,8 @@
 
                         <WorkingExperienceForm></WorkingExperienceForm>
                         <div align='right'>
-                            <b-button type='reset' variant='danger' style='width: 250px'> Cancel </b-button>
-                            <b-button type='submit' variant='outline-success' style='width: 250px'> Create </b-button>
+                            <b-button type='reset' variant='danger' style='width: 250px'>Cancel</b-button>
+                            <b-button type='submit' variant='outline-success' style='width: 250px'>Create</b-button>
                         </div>
                     </b-form>
                 </b-card-body>
@@ -30,7 +30,7 @@
 import PersonalInformationForm from './application/PersonalInformationForm'
 import EducationForm from './application/EducationForm'
 import WorkingExperienceForm from './application/WorkingExperienceForm'
-import EmployeeApplicationService from '../service/EmployeeApplicationService.js'
+// import EmployeeApplicationService from '../service/EmployeeApplicationService.js'
 
 export default {
   name: 'ApplicationForm',
@@ -41,15 +41,12 @@ export default {
   },
   data () {
     return {
-      show: true
     }
   },
   methods: {
     onSubmit (evt) {
       evt.preventDefault()
-      var application = JSON.stringify(this.form)
-      alert(application)
-      EmployeeApplicationService.create(application)
+      // EmployeeApplicationService.create(application)
     },
     onReset (evt) {
       evt.preventDefault()
